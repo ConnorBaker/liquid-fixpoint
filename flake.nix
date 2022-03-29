930cf697c2e2aa9c3c2412d8eadcae8b06159987
Connor Baker <connorbaker01@gmail.com>
2022-03-29 18:54:28 -0400

    ci: Switch to cabal

--- flake.nix
+++ flake.nix
@@ -3,19 +3,23 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.rest-rewrite.url = "github:connorbaker/rest/eb12a7107ec98e2b06cf33777479148514aff4ce";
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
+    , rest-rewrite
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
+      compiler = "ghc8107";
       pkgs = nixpkgs.legacyPackages.${system};
+      rest-rewrite-deps = rest-rewrite.devShells.${system}.default.nativeBuildInputs;
     in
     {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ stack ];
+        nativeBuildInputs = with pkgs; [ haskell.compiler.${compiler} cabal-install z3 ] ++ rest-rewrite-deps;
         NIX_PATH = "nixpkgs=${nixpkgs}";
       };
     });
